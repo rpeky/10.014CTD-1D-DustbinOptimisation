95edb7eeab7b541a7322fa5c22328fbb7d564096
Ryan Pek <ryanpekyize2001@gmail.com>
2023-11-09 15:00:07 +0800

    added stuff

---
+++ 10.014CTD-1D-DustbinOptimisation/Graph.py
@@ -0,0 +1,21 @@
+
+
+class Graph():
+    def __init__(self):
+        self.testattribute=None
+        
+    def return_knownfloorplangraph(self):
+        pass
+    
+    def create_floorplan(self):
+        pass
+
+    def add_vertex(self, vert_id):
+        pass
+    
+    def add_edge(self, vert_origin, vert_dest, weight):
+        pass
+    
+    
+        
+
